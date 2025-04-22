4395e39cc655cc93200236537bb0814b12fa351d
Lucas <lucaslip2005@gmail.com>
2025-04-22 17:10:18 -0300

    FormataÃcao do tempo

--- public/script.js
+++ public/script.js
@@ -1,15 +1,32 @@
 const socket = io()
 
 socket.on("tempo", (t) => {
-    document.getElementById("tempoGeral").textContent = t
+    document.getElementById("tempoGeral").textContent = formatarTempo(t)
 })
 
 socket.on("tempoTime", (nome, tempo) => {
-    document.getElementById(`tempo_${nome}`).textContent = tempo
+    // document.getElementById(`tempo_${nome}`).textContent = tempo
+
+    document.getElementById(`tempo_${nome}`).textContent = formatarTempo(tempo)
 })
+
+const formatarTempo = (tempo) => {
+    const horas = Math.floor(tempo / 3600)
+    const minutos = Math.floor((tempo % 3600) / 60)
+    const segundos = Math.floor(tempo % 60)
+    const milissegundos = parseFloat((tempo % 1).toFixed(3)) * 1000
+
+    const format = String(minutos).padStart(2, "0") + ":" +
+        String(segundos).padStart(2, "0") + "." +
+        String(milissegundos).padStart(3, "0")
+    // return `${minutos}:${segundos}:${milissegundos}`
+    return format
+}
 
 const construir = (nome, nVoltas) => {
     const container = document.createElement("div")
+
+    nVoltas = String(nVoltas).padStart(2, "0")
 
     container.innerHTML = `
         <div style="border: 1px solid black;" class="time" onclick="adicionar('${nome}')">
@@ -40,6 +57,8 @@ const adicionar = (nome) => {
             document.getElementById("volta_" + nome).textContent = `Voltas: ${resp}`
         })
 }
+
+
 
 fetch("/pegarDados").
     then((response) => {
